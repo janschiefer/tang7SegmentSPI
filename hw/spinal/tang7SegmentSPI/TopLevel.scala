692361832f58aa6f263ee38458b2f0100a109e7c
Jan Schiefer <janschiefer@users.noreply.github.com>
2024-02-17 13:12:16 +0100

    Decision

--- hw/spinal/tang7SegmentSPI/TopLevel.scala
+++ hw/spinal/tang7SegmentSPI/TopLevel.scala
@@ -6,14 +6,21 @@ import spinal.lib.fsm._
 import spinal.lib.com.spi.SpiMaster
 import spinal.lib._
 
-
 // Hardware definition
 case class TopLevel() extends Component {
+
   val io = new Bundle {
     val spi_out = master(SpiMaster())
 
     val rst = in Bool ()
   }
+
+   def sendSPIData(data: Bits, counter: UInt, slowDownFactor : Int ) : Unit = {
+    this.io.spi_out.ss(0) := False
+    this.io.spi_out.sclk := counter(slowDownFactor)
+    this.io.spi_out.mosi := data.asBools.reverse((counter >> 1+slowDownFactor).resized)
+  }
+
   io.spi_out.mosi.setName("seven_segm_mosi")
   io.spi_out.ss.setName("seven_segm_ss")
   io.spi_out.sclk.setName("seven_segm_sclk")
@@ -55,130 +62,76 @@ case class TopLevel() extends Component {
 
     val fsm = new StateMachine{ 
     
-    val INITIAL, SCAN_LIMIT, SET_DECODE, SET_INTENSITY, SET_NOTEST, SET_NOSHUTDOWN, SET_DIGIT_ZERO, IDLE1,IDLE2,IDLE3,IDLE4,IDLE5,IDLE6 = State()
+    val INITIAL, CONFIGURATION, WAIT, SET_DIGIT = State()
     
     setEntry(INITIAL)
 
     val counter = Reg(UInt(7 + slowDownFactor bits))
+    val configuration_stage = RegInit(U(0, 3 bits))
+    val run_stage = RegInit(False)
+
     counter := counter + 1
 
     //Scan all digits
     INITIAL.whenIsActive{
-        goto(SCAN_LIMIT)
+        configuration_stage := 0
+        run_stage := False
+        goto(CONFIGURATION)
     }
 
     //Display digit 0 only
-    SCAN_LIMIT.onEntry(counter := 0)
-    SCAN_LIMIT.whenIsActive{
+    CONFIGURATION.onEntry(counter := 0)
+    CONFIGURATION.whenIsActive {
+
+      val configuration_bitstream = configuration_stage.mux(
+        0 -> B"0000" ## B"1011" ## B"00000111", //Scan limit - display all digits
+        1 -> B"0000" ## B"1001" ## B"11111111", //Decode mode - all digits
+        2 -> B"0000" ## B"1010" ## B"00001111", //Intensity register - full brightness
+        3 -> B"0000" ## B"1111" ## B"00000000", //Display test register - no, normal operation
+        4 -> B"0000" ## B"1100" ## B"00000001", //Shutdown register - no, normal operation
+        default -> B"0000" ## B"0000" ## B"00000000", //No operation
+      )
       io.spi_out.ss(0) := False
       io.spi_out.sclk := counter(slowDownFactor)
-      val bitstream = B"0000" ## B"1011" ## B"00000000"
-      io.spi_out.mosi := bitstream.asBools.reverse((counter >> 1+slowDownFactor).resized)
-      when(counter === (widthOf(bitstream)*2 << slowDownFactor)-1){
-        goto(IDLE1)
+      io.spi_out.mosi := configuration_bitstream.asBools.reverse((counter >> 1+slowDownFactor).resized)
+      when(counter === (widthOf(configuration_bitstream)*2 << slowDownFactor)-1) {
+        configuration_stage := configuration_stage + U(1).resized
+        goto(WAIT)
       }
     }
 
-    IDLE1.onEntry(counter := 0)
-    IDLE1.whenIsActive{
-      when(counter === 300){
-        goto(SET_DECODE)
-      }   
-    }
+    WAIT.onEntry(counter := 0)
+    WAIT.whenIsActive{
+      when(counter === 300) {
+        
+        when (run_stage === False) {
 
-    //Decode all digits
-    SET_DECODE.onEntry(counter := 0)
-    SET_DECODE.whenIsActive{
-      io.spi_out.ss(0) := False
-      io.spi_out.sclk := counter(slowDownFactor)
-      val bitstream = B"0000" ## B"1001" ## B"11111111"
-      io.spi_out.mosi := bitstream.asBools.reverse((counter >> 1+slowDownFactor).resized)
-      when(counter === (widthOf(bitstream)*2 << slowDownFactor)-1){
-        goto(IDLE2)
+        when (configuration_stage > 4) {
+          run_stage := True
+          goto(SET_DIGIT)
+        }
+        .otherwise {
+          goto(CONFIGURATION)
+        }
+
       }
-    }
-
-    IDLE2.onEntry(counter := 0)
-    IDLE2.whenIsActive{
-      when(counter === 300){
-        goto(SET_INTENSITY)
-      }   
-    }
-
-    //Full brightness
-    SET_INTENSITY.onEntry(counter := 0)
-    SET_INTENSITY.whenIsActive{
-      io.spi_out.ss(0) := False
-      io.spi_out.sclk := counter(slowDownFactor)
-      val bitstream = B"0000" ## B"1010" ## B"00001111"
-      io.spi_out.mosi := bitstream.asBools.reverse((counter >> 1+slowDownFactor).resized)
-      when(counter === (widthOf(bitstream)*2 << slowDownFactor)-1){
-        goto(IDLE3)
+      .otherwise {
+        goto(SET_DIGIT)
       }
-    }
 
-    IDLE3.onEntry(counter := 0)
-    IDLE3.whenIsActive{
-      when(counter === 300){
-        goto(SET_NOTEST)
-      }   
-    }
-
-    //NOT test mode
-    SET_NOTEST.onEntry(counter := 0)
-    SET_NOTEST.whenIsActive{
-      io.spi_out.ss(0) := False
-      io.spi_out.sclk := counter(slowDownFactor)
-      val bitstream = B"0000" ## B"1111" ## B"00000000"
-      io.spi_out.mosi := bitstream.asBools.reverse((counter >> 1+slowDownFactor).resized)
-      when(counter === (widthOf(bitstream)*2 << slowDownFactor)-1){
-        goto(IDLE4)
-      }
-    }
-
-    IDLE4.onEntry(counter := 0)
-    IDLE4.whenIsActive{
-      when(counter === 300){
-        goto(SET_NOSHUTDOWN)
-      }   
-    }
-
-    //NOT shutdown mode
-    SET_NOSHUTDOWN.onEntry(counter := 0)
-    SET_NOSHUTDOWN.whenIsActive{
-      io.spi_out.ss(0) := False
-      io.spi_out.sclk := counter(slowDownFactor)
-      val bitstream = B"0000" ## B"1100" ## B"00000001"
-      io.spi_out.mosi := bitstream.asBools.reverse((counter >> 1+slowDownFactor).resized)
-      when(counter === (widthOf(bitstream)*2 << slowDownFactor)-1){
-        goto(IDLE5)
-      }
-    }
-
-    IDLE5.onEntry(counter := 0)
-    IDLE5.whenIsActive{
-      when(counter === 300){
-        goto(SET_DIGIT_ZERO)
       }   
     }
 
     //Dispay number 4 on digit 0
-    SET_DIGIT_ZERO.onEntry(counter := 0)
-    SET_DIGIT_ZERO.whenIsActive{
+    SET_DIGIT.onEntry(counter := 0)
+    SET_DIGIT.whenIsActive {
       io.spi_out.ss(0) := False
       io.spi_out.sclk := counter(slowDownFactor)
       val bitstream = B"0000" ## B"0001" ## B"00000100"
       io.spi_out.mosi := bitstream.asBools.reverse((counter >> 1+slowDownFactor).resized)
       when(counter === (widthOf(bitstream)*2 << slowDownFactor)-1){
-        goto(IDLE6)
+        goto(WAIT)
       }
-    }
-
-    IDLE6.onEntry(counter := 0)
-    IDLE6.whenIsActive{
-      when(counter === 300){
-        goto(SCAN_LIMIT)
-      }   
     }
     
    
