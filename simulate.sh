78e1409f95a3652939927af1c051aab2f06b5897
Jan Schiefer <janschiefer@users.noreply.github.com>
2024-02-18 15:07:20 +0100

    Update sbt to 1.9.0 and Java to 21

--- simulate.sh
+++ simulate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ~/.sdkman/bin/sdkman-init.sh
-sdk use java 17.0.10-tem
+sdk use java java 21.0.2-tem
 source ~/oss-cad-suite/environment
 sbt "runMain tang7SegmentSPI.TopLevelSim"
 gtkwave simulation.gtkw&
